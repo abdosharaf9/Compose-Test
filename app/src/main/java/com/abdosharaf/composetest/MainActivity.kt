2136f2246f3bd10f588612e26eac425e4f0f1946
Abdo Sharaf <bodyasharaf8@gmail.com>
2023-11-07 02:44:35 +0200

    Commit #3

--- app/src/main/java/com/abdosharaf/composetest/MainActivity.kt
+++ app/src/main/java/com/abdosharaf/composetest/MainActivity.kt
@@ -1,79 +1,104 @@
 package com.abdosharaf.composetest
 
-import android.app.Application
-import android.content.Context
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredWidth
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Devices
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Test(this)
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.SpaceAround,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Box(modifier = Modifier.fillMaxWidth(0.5f)) {
+                    ImageCard(
+                        painter = painterResource(id = R.drawable.my_photo),
+                        contentDescription = "Photo of me",
+                        text = "Abdo Sharaf"
+                    )
+                }
+
+                Box(modifier = Modifier.fillMaxWidth(0.5f)) {
+                    ImageCard(
+                        painter = painterResource(id = R.drawable.my_photo_2),
+                        contentDescription = "Photo of me",
+                        text = "Abdo & 3mor"
+                    )
+                }
+            }
         }
     }
 }
 
 @Composable
-fun Test(context: Context) {
-    Column(
-        modifier = Modifier
-            .background(Color.Green)
-            .fillMaxHeight(0.7f)
-            .fillMaxWidth()
-            .border(5.dp, Color.Red)
-            .padding(16.dp)
-            .border(5.dp, Color.Yellow)
-            .padding(16.dp)
-            .border(5.dp, Color.Blue)
-            .padding(16.dp)
-            .border(5.dp, Color.Black)
-            .padding(16.dp)
-//            .width(700.dp)
-//            .requiredWidth(700.dp)
-//            .padding(vertical = 20.dp, horizontal = 25.dp)
-//            .padding(top = 50.dp, start = 20.dp, end = 30.dp, bottom = 60.dp)
-    ) {
-//        Text(text = "Abdo", modifier = Modifier.offset(0.dp, 100.dp))
-        Text(
-            text = "Abdo", modifier = Modifier
-                .border(5.dp, Color.Magenta)
-                .offset(20.dp, 20.dp)
-                .border(10.dp, Color.Gray)
-                .padding(40.dp)
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(20.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 15.dp
         )
-        Spacer(modifier = Modifier.height(50.dp))
-        Text(text = "Sharaf", modifier = Modifier.clickable {
-            Toast.makeText(context, "Clicked!", Toast.LENGTH_SHORT).show()
-        })
-    }
-}
+    ) {
+        Box(
+            modifier = Modifier.height(300.dp)
+        ) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
 
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun GreetingPreview() {
-//    Test()
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(Color.Transparent, Color.Black),
+                            startY = 300f
+                        )
+                    )
+            )
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(text = text, style = TextStyle(color = Color.White, fontSize = 16.sp))
+            }
+        }
+    }
 }
