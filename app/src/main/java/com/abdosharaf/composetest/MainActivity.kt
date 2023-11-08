58e6f12fa3cd0ee570a11c232feb63130417cf8a
Abdo Sharaf <bodyasharaf8@gmail.com>
2023-11-08 18:28:56 +0200

    Commit #5

--- app/src/main/java/com/abdosharaf/composetest/MainActivity.kt
+++ app/src/main/java/com/abdosharaf/composetest/MainActivity.kt
@@ -4,69 +4,100 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.sp
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val tajwalFontFamily = FontFamily(
-            Font(resId = R.font.tajawal_light, weight = FontWeight.Light),
-            Font(resId = R.font.tajawal_medium, weight = FontWeight.Medium),
-            Font(resId = R.font.tajawal_regular, weight = FontWeight.Normal),
-            Font(resId = R.font.tajawal_bold, weight = FontWeight.Bold),
-        )
-
         setContent {
-            Box(
-                modifier = Modifier
-                    .background(Color(0xFF101010))
-                    .fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(
-                    text = buildAnnotatedString {
-                        withStyle(style = SpanStyle(
-                            color = Color.Blue,
-                            fontSize = 40.sp
-                        )) {
-                            append("A")
-                        }
+//            ColorBox(modifier = Modifier.fillMaxSize())
 
-                        append("bdo ")
+            val color = remember {
+                mutableStateOf(Color.Red)
+            }
 
-                        withStyle(style = SpanStyle(
-                            color = Color.Blue,
-                            fontSize = 40.sp
-                        )) {
-                            append("S")
-                        }
+            Column(modifier = Modifier.fillMaxSize()) {
+                ColorBoxV2(
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxSize()
+                ) {
+                    color.value = it
+                }
 
-                        append("haraf")
-                    },
-                    color = Color.White,
-                    fontSize = 30.sp,
-                    fontFamily = tajwalFontFamily,
-                    fontWeight = FontWeight.Bold,
-                    fontStyle = FontStyle.Italic,
-                    textAlign = TextAlign.Center,
-                    textDecoration = TextDecoration.Underline
+                Box(
+                    modifier = Modifier
+                        .weight(2f)
+                        .fillMaxSize()
+                        .background(color.value)
                 )
             }
         }
+    }
+}
+
+@Composable
+fun ColorBox(modifier: Modifier = Modifier) {
+    val color = remember {
+        mutableStateOf(Color.Yellow)
+    }
+
+    Box(modifier = modifier
+        .background(color.value)
+        .clickable(
+            indication = null,
+            interactionSource = remember { MutableInteractionSource() }
+        ) {
+            color.value = Color(
+                Random.nextFloat(),
+                Random.nextFloat(),
+                Random.nextFloat()
+            )
+        }, contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Click here to change the color!!",
+            fontSize = 30.sp,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Composable
+fun ColorBoxV2(modifier: Modifier = Modifier, onClick: (Color) -> Unit) {
+    Box(modifier = modifier
+        .background(Color.Yellow)
+        .clickable(
+            indication = null,
+            interactionSource = remember { MutableInteractionSource() }
+        ) {
+            onClick(
+                Color(
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    Random.nextFloat()
+                )
+            )
+        }, contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Click here to change the color!!",
+            fontSize = 30.sp,
+            textAlign = TextAlign.Center
+        )
     }
 }
