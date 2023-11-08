bea8a175edfe231fb01183d73c1aaf1af21a4291
Abdo Sharaf <bodyasharaf8@gmail.com>
2023-11-08 13:42:08 +0200

    Commit #4

--- app/src/main/java/com/abdosharaf/composetest/MainActivity.kt
+++ app/src/main/java/com/abdosharaf/composetest/MainActivity.kt
@@ -3,101 +3,69 @@ package com.abdosharaf.composetest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.SpaceAround,
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Box(modifier = Modifier.fillMaxWidth(0.5f)) {
-                    ImageCard(
-                        painter = painterResource(id = R.drawable.my_photo),
-                        contentDescription = "Photo of me",
-                        text = "Abdo Sharaf"
-                    )
-                }
-
-                Box(modifier = Modifier.fillMaxWidth(0.5f)) {
-                    ImageCard(
-                        painter = painterResource(id = R.drawable.my_photo_2),
-                        contentDescription = "Photo of me",
-                        text = "Abdo & 3mor"
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun ImageCard(
-    painter: Painter,
-    contentDescription: String,
-    text: String,
-    modifier: Modifier = Modifier
-) {
-    Card(
-        modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(20.dp),
-        elevation = CardDefaults.cardElevation(
-            defaultElevation = 15.dp
+        val tajwalFontFamily = FontFamily(
+            Font(resId = R.font.tajawal_light, weight = FontWeight.Light),
+            Font(resId = R.font.tajawal_medium, weight = FontWeight.Medium),
+            Font(resId = R.font.tajawal_regular, weight = FontWeight.Normal),
+            Font(resId = R.font.tajawal_bold, weight = FontWeight.Bold),
         )
-    ) {
-        Box(
-            modifier = Modifier.height(300.dp)
-        ) {
-            Image(
-                painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Crop
-            )
 
+        setContent {
             Box(
                 modifier = Modifier
-                    .fillMaxSize()
-                    .background(
-                        brush = Brush.verticalGradient(
-                            colors = listOf(Color.Transparent, Color.Black),
-                            startY = 300f
-                        )
-                    )
-            )
-
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(12.dp),
-                contentAlignment = Alignment.BottomStart
+                    .background(Color(0xFF101010))
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
             ) {
-                Text(text = text, style = TextStyle(color = Color.White, fontSize = 16.sp))
+                Text(
+                    text = buildAnnotatedString {
+                        withStyle(style = SpanStyle(
+                            color = Color.Blue,
+                            fontSize = 40.sp
+                        )) {
+                            append("A")
+                        }
+
+                        append("bdo")
+
+                        withStyle(style = SpanStyle(
+                            color = Color.Blue,
+                            fontSize = 40.sp
+                        )) {
+                            append("S")
+                        }
+
+                        append("haraf")
+                    },
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    fontFamily = tajwalFontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontStyle = FontStyle.Italic,
+                    textAlign = TextAlign.Center,
+                    textDecoration = TextDecoration.Underline
+                )
             }
         }
     }
