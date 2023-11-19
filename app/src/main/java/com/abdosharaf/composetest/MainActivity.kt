a67e3d4e30fad17bb90ea8eb1136537436c2688c
Abdo Sharaf <bodyasharaf8@gmail.com>
2023-11-20 00:12:51 +0200

    Commit #10

--- app/src/main/java/com/abdosharaf/composetest/MainActivity.kt
+++ app/src/main/java/com/abdosharaf/composetest/MainActivity.kt
@@ -3,86 +3,84 @@ package com.abdosharaf.composetest
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.ComponentActivity
-import androidx.compose.animation.animateColor
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var boxSizeState by remember {
-                mutableStateOf(200.dp)
-            }
-
-            val size by animateDpAsState(
-                targetValue = boxSizeState,
-                /*tween(
-                    durationMillis = 3000,
-                    delayMillis = 0,
-                    easing = FastOutSlowInEasing
-                )*/
-
-                /*spring(
-                    dampingRatio = Spring.DampingRatioHighBouncy
-                )*/
-
-                /*keyframes {
-                    durationMillis = 3000
-                    boxSizeState at 0 with LinearEasing
-                    boxSizeState * 1.5f at 1000 with FastOutSlowInEasing
-                    boxSizeState * 2f at 2000 with FastOutLinearInEasing
-                    boxSizeState * 3f at 3000 with LinearOutSlowInEasing
-                }*/
-                tween(
-                    durationMillis = 1500,
-                    delayMillis = 100
-                ), label = ""
-            )
-
-            val infiniteTransition = rememberInfiniteTransition(label = "")
-            val boxColor by infiniteTransition.animateColor(
-                initialValue = Color.Green,
-                targetValue = Color.Red,
-                animationSpec = infiniteRepeatable(
-                    animation = tween(
-                        durationMillis = 3000
-                    ),
-                    repeatMode = RepeatMode.Reverse
-                ), label = ""
-            )
-
             Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                Box(
-                    modifier = Modifier
-                        .size(size)
-                        .background(boxColor),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Button(onClick = {
-                        boxSizeState += 50.dp
-                    }) {
-                        Text(text = "Increase Box Size")
-                    }
-                }
+                CircularProgressBar(percentage = 0.7f, number = 100)
             }
         }
+    }
+}
+
+@Composable
+fun CircularProgressBar(
+    percentage: Float,
+    number: Int,
+    fontSize: TextUnit = 20.sp,
+    radius: Dp = 50.dp,
+    color: Color = Color.Green,
+    strokeWidth: Dp = 8.dp,
+    animationDuration: Int = 1000,
+    animationDelay: Int = 0
+) {
+    var animationPlayed by remember {
+        mutableStateOf(false)
+    }
+
+    val currentPercent = animateFloatAsState(
+        targetValue = if (animationPlayed) percentage else 0f,
+        animationSpec = tween(
+            durationMillis = animationDuration,
+            delayMillis = animationDelay
+        ), label = ""
+    )
+
+    LaunchedEffect(key1 = true) {
+        animationPlayed = true
+    }
+
+    Box(contentAlignment = Alignment.Center) {
+        Canvas(modifier = Modifier.size(radius * 2f)) {
+            drawArc(
+                color = color,
+                startAngle = -90f,
+                sweepAngle = 360 * currentPercent.value,
+                useCenter = false,
+                style = Stroke(width = strokeWidth.toPx(), cap = StrokeCap.Round)
+            )
+        }
+
+        Text(
+            text = "${(currentPercent.value * number).toInt()}%",
+            color = Color.Black,
+            fontSize = fontSize,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
