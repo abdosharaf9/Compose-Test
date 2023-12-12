34106dc9a0a4ccedc7829f6b971a6e9598283b0d
Abdo Sharaf <bodyasharaf8@gmail.com>
2023-12-12 05:38:28 +0200

    Commit #15

--- app/src/main/java/com/abdosharaf/composetest/Navigation.kt
+++ app/src/main/java/com/abdosharaf/composetest/Navigation.kt
@@ -1,99 +1,77 @@
 package com.abdosharaf.composetest
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
+import kotlinx.coroutines.delay
 
 @Composable
 fun Navigation() {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = Screen.MainScreen.route) {
-        composable(
-            route = Screen.MainScreen.route
-        ) {
-            MainScreen(navController)
+    NavHost(navController = navController, startDestination = "splash_screen") {
+        composable(route = "splash_screen") {
+            SplashScreen(navController)
         }
 
-        composable(
-            route = Screen.DetailsScreen.route + "/{name}",
-            arguments = listOf(
-                navArgument(name = "name") {
-                    type = NavType.StringType
-                    defaultValue = "Abdo"
-                    nullable = true
+        composable(route = "main_screen") {
+            MainScreen()
+        }
+    }
+}
+
+@Composable
+fun SplashScreen(navController: NavController) {
+    val scale = remember {
+        Animatable(initialValue = 0f)
+    }
+
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            targetValue = 1.5f,
+            animationSpec = tween(
+                durationMillis = 500,
+                easing = {
+                    OvershootInterpolator(2f).getInterpolation(it)
                 }
             )
-        ) { entry ->
-            DetailsScreen(name = entry.arguments?.getString("name"))
-        }
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun MainScreen(navController: NavController) {
-    var text by remember {
-        mutableStateOf("")
-    }
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.LightGray)
-            .padding(horizontal = 50.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        TextField(
-            value = text,
-            onValueChange = { text = it },
-            modifier = Modifier.fillMaxWidth()
         )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(onClick = {
-            navController.navigate(Screen.DetailsScreen.withArgs(text))
-        }, modifier = Modifier.align(Alignment.End)) {
-            Text(text = "Go to details screen")
-        }
+        delay(1500L)
+        navController.navigate("main_screen")
     }
-}
 
-@Composable
-fun DetailsScreen(name: String?) {
     Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.LightGray)
-            .padding(horizontal = 50.dp),
+        modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Text(text = "Hello, $name!")
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "Logo",
+            modifier = Modifier.scale(scale.value)
+        )
+    }
+}
+
+@Composable
+fun MainScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Main Screen")
     }
 }
