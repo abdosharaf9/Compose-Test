3398bc27527637f9e9f12d7506a16386787a244c
Abdo Sharaf <bodyasharaf8@gmail.com>
2023-12-13 23:50:11 +0200

    Commit #20

--- app/src/main/java/com/abdosharaf/composetest/MainActivity.kt
+++ app/src/main/java/com/abdosharaf/composetest/MainActivity.kt
@@ -1,27 +1,14 @@
 package com.abdosharaf.composetest
 
-import android.Manifest
+import android.graphics.Color
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.TextView
 import androidx.activity.compose.setContent
 import androidx.activity.ComponentActivity
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
-import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.PermissionState
-import com.google.accompanist.permissions.rememberMultiplePermissionsState
+import androidx.compose.ui.viewinterop.AndroidView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,90 +25,14 @@ fun Test() {
     HomeScreen()
 }
 
-@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun HomeScreen() {
-    val permissionState = rememberMultiplePermissionsState(
-        permissions = listOf(
-            Manifest.permission.RECORD_AUDIO,
-            Manifest.permission.CAMERA,
-        )
-    )
-
-    val lifecycleOwner = LocalLifecycleOwner.current
-    DisposableEffect(key1 = lifecycleOwner, effect = {
-        val observer = LifecycleEventObserver { _, event ->
-            if (event == Lifecycle.Event.ON_START) {
-                permissionState.launchMultiplePermissionRequest()
-            }
-        }
-
-        lifecycleOwner.lifecycle.addObserver(observer)
-
-        onDispose {
-            lifecycleOwner.lifecycle.removeObserver(observer)
-        }
-    })
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(horizontal = 20.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        permissionState.permissions.forEach { perm ->
-            when (perm.permission) {
-                Manifest.permission.CAMERA -> {
-                    val text = when {
-                        perm.hasPermission -> {
-                            "Camera permission accepted"
-                        }
-
-                        perm.shouldShowRationale -> {
-                            "Camera permission is needed to access the camera"
-                        }
-
-                        perm.isPermanentlyDenied() -> {
-                            "Camera permission was permanently denied. You can enable it in the app settings."
-                        }
-
-                        else -> {
-                            "Camera permission is needed to access the camera"
-                        }
-                    }
-                    Text(text = text, textAlign = TextAlign.Center)
-                }
-
-                Manifest.permission.RECORD_AUDIO -> {
-                    val text = when {
-                        perm.hasPermission -> {
-                            "Record audio permission accepted"
-                        }
-
-                        perm.shouldShowRationale -> {
-                            "Record audio permission is needed to access the microphone"
-                        }
-
-                        perm.isPermanentlyDenied() -> {
-                            "Record audio permission was permanently denied. You can enable it in the app settings."
-                        }
-
-                        else -> {
-                            "Record audio permission is needed to access the microphone"
-                        }
-                    }
-                    Text(
-                        text = text,
-                        modifier = Modifier.padding(top = 16.dp),
-                        textAlign = TextAlign.Center
-                    )
-                }
-            }
+    AndroidView(factory = { TextView(it) }) { tvTest ->
+        tvTest.apply {
+            text = "Hello using XML!!"
+            setTextColor(Color.BLACK)
+            textSize = 20f
+            gravity = Gravity.CENTER
         }
     }
 }
-
-@OptIn(ExperimentalPermissionsApi::class)
-fun PermissionState.isPermanentlyDenied() =
-    !hasPermission && !shouldShowRationale && permissionRequested
