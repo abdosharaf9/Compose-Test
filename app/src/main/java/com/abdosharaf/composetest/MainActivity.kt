209151036046de57071cba3c5dd8bd468f36cc83
Abdo Sharaf <bodyasharaf8@gmail.com>
2023-12-16 03:27:13 +0200

    Commit #27

--- app/src/main/java/com/abdosharaf/composetest/MainActivity.kt
+++ app/src/main/java/com/abdosharaf/composetest/MainActivity.kt
@@ -1,33 +1,41 @@
 package com.abdosharaf.composetest
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.activity.ComponentActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.ExitToApp
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Info
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HomeScreen(appKiller = {
-                finish()
-            })
+            HomeScreen()
         }
     }
 }
@@ -35,69 +43,69 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun Test() {
-    HomeScreen(appKiller = {
-        println("App is closed")
-    })
+    HomeScreen()
 }
 
 @Composable
-fun HomeScreen(appKiller: () -> Unit) {
-    val drawerList = listOf(
-        MenuItem(
-            title = "Home",
-            icon = Icons.Default.Home,
-            contentDescription = "Got to Home",
-            id = "home"
-        ),
-        MenuItem(
-            title = "Settings",
-            icon = Icons.Default.Settings,
-            contentDescription = "Got to Settings",
-            id = "settings"
-        ),
-        MenuItem(
-            title = "Profile",
-            icon = Icons.Default.AccountCircle,
-            contentDescription = "Got to Profile",
-            id = "profile"
-        ),
-        MenuItem(
-            title = "Info",
-            icon = Icons.Default.Info,
-            contentDescription = "Got to Info",
-            id = "info"
-        ),
-        MenuItem(
-            title = "Exit",
-            icon = Icons.Default.ExitToApp,
-            contentDescription = "Exit the app",
-            id = "exit"
+fun HomeScreen() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        val state = rememberLazyGridState(
+//        initialFirstVisibleItemIndex = 80
         )
-    )
-    val context = LocalContext.current
-    val scaffoldState = rememberScaffoldState()
-    val scope = rememberCoroutineScope()
+        val scope = rememberCoroutineScope()
 
-    Scaffold(
-        scaffoldState = scaffoldState,
-        topBar = {
-            AppBar(onIconClicked = {
-                scope.launch {
-                    scaffoldState.drawerState.open()
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(100.dp),/*.Fixed(5)*/
+            state = state,
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            items(100) {
+                Box(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .aspectRatio(1f)
+                        .clip(RoundedCornerShape(5.dp))
+                        .background(Color.Green),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(text = "Item ${it + 1}")
                 }
-            })
-        },
-        drawerContent = {
-            DrawerHeader()
-            DrawerBody(items = drawerList, onItemClicked = {
-                if (it.id == "exit") {
-                    appKiller()
-                } else {
-                    Toast.makeText(context, it.contentDescription, Toast.LENGTH_SHORT).show()
-                }
-            })
+            }
         }
-    ) {
-        Box(modifier = Modifier.padding(it))
+
+        Column(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(end = 16.dp, bottom = 16.dp)
+                .clip(RoundedCornerShape(20.dp))
+                .border(width = 1.dp, color = Color.Gray, shape = RoundedCornerShape(20.dp))
+                .background(Color.White)
+        ) {
+            IconButton(onClick = {
+                scope.launch {
+                    state.animateScrollToItem(index = 0)
+                }
+            }) {
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowUp,
+                    contentDescription = "Go to the list top",
+                    tint = Color.Black,
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+
+            IconButton(onClick = {
+                scope.launch {
+                    state.animateScrollToItem(index = 99)
+                }
+            }) {
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowDown,
+                    contentDescription = "Go to the list bottom",
+                    tint = Color.Black,
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+        }
     }
 }
