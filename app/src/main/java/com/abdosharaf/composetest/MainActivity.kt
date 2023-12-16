71eb0a6865b4a25972eb80170cacabc7ecc42544
Abdo Sharaf <bodyasharaf8@gmail.com>
2023-12-16 06:36:15 +0200

    Commit #29

--- app/src/main/java/com/abdosharaf/composetest/MainActivity.kt
+++ app/src/main/java/com/abdosharaf/composetest/MainActivity.kt
@@ -1,24 +1,33 @@
 package com.abdosharaf.composetest
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.ComponentActivity
-import androidx.compose.foundation.layout.Box
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
-import androidx.navigation.navDeepLink
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,35 +46,63 @@ fun Test() {
 
 @Composable
 fun HomeScreen() {
-    val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = "home") {
-        composable(route = "home") {
-            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                Button(onClick = { navController.navigate("details") }) {
-                    Text(text = "Go to details", color = Color.White)
-                }
-            }
+    var count by remember {
+        mutableIntStateOf(0)
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        AnimatedCounter(count = count, textStyle = MaterialTheme.typography.h2)
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {
+            count++
+        }) {
+            Text(text = "Increase the counter", color = Color.White)
         }
+    }
+}
 
-        composable(
-            route = "details",
-            deepLinks = listOf(
-                navDeepLink {
-                    uriPattern = "https://abdosharaf.com/{id}"
-                    action = Intent.ACTION_VIEW
-                }
-            ),
-            arguments = listOf(
-                navArgument(name = "id") {
-                    type = NavType.IntType
-                    defaultValue = -1
-                }
-            )
-        ) { entry ->
-            val id = entry.arguments?.getInt("id")
+@Composable
+fun AnimatedCounter(count: Int, textStyle: TextStyle, modifier: Modifier = Modifier) {
+    var oldCount by remember {
+        mutableIntStateOf(count)
+    }
 
-            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                Text(text = "The passed number: $id")
+    SideEffect {
+        oldCount = count
+    }
+
+    Row(modifier = modifier) {
+        val oldCountString = oldCount.toString()
+        val newCountString = count.toString()
+
+        for (i in newCountString.indices) {
+            val oldChar = oldCountString.getOrNull(i)
+            val newChar = newCountString[i]
+
+            val char = if (oldChar == newChar) {
+                oldCountString[i]
+            } else {
+                newCountString[i]
+            }
+
+            AnimatedContent(
+                targetState = char,
+                label = "",
+                transitionSpec = {
+                    slideInVertically { it } togetherWith slideOutVertically { -it }
+                }
+            ) { text ->
+                Text(
+                    text = text.toString(),
+                    style = textStyle,
+                    softWrap = false
+                )
             }
         }
     }
