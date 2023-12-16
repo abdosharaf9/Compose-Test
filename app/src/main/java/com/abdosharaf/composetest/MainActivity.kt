5611671e6765d8806292bc50603e68c53e10d146
Abdo Sharaf <bodyasharaf8@gmail.com>
2023-12-16 04:25:48 +0200

    Commit #28

--- app/src/main/java/com/abdosharaf/composetest/MainActivity.kt
+++ app/src/main/java/com/abdosharaf/composetest/MainActivity.kt
@@ -1,35 +1,24 @@
 package com.abdosharaf.composetest
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.ComponentActivity
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.rememberLazyGridState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.launch
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import androidx.navigation.navDeepLink
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,63 +37,35 @@ fun Test() {
 
 @Composable
 fun HomeScreen() {
-    Box(modifier = Modifier.fillMaxSize()) {
-        val state = rememberLazyGridState(
-//        initialFirstVisibleItemIndex = 80
-        )
-        val scope = rememberCoroutineScope()
-
-        LazyVerticalGrid(
-            columns = GridCells.Adaptive(100.dp),/*.Fixed(5)*/
-            state = state,
-            modifier = Modifier.fillMaxSize(),
-        ) {
-            items(100) {
-                Box(
-                    modifier = Modifier
-                        .padding(8.dp)
-                        .aspectRatio(1f)
-                        .clip(RoundedCornerShape(5.dp))
-                        .background(Color.Green),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Text(text = "Item ${it + 1}")
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "home") {
+        composable(route = "home") {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Button(onClick = { navController.navigate("details") }) {
+                    Text(text = "Go to details", color = Color.White)
                 }
             }
         }
 
-        Column(
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .padding(end = 16.dp, bottom = 16.dp)
-                .clip(RoundedCornerShape(20.dp))
-                .border(width = 1.dp, color = Color.Gray, shape = RoundedCornerShape(20.dp))
-                .background(Color.White)
-        ) {
-            IconButton(onClick = {
-                scope.launch {
-                    state.animateScrollToItem(index = 0)
+        composable(
+            route = "details",
+            deepLinks = listOf(
+                navDeepLink {
+                    uriPattern = "https://abdosharaf.com/{id}"
+                    action = Intent.ACTION_VIEW
                 }
-            }) {
-                Icon(
-                    imageVector = Icons.Default.KeyboardArrowUp,
-                    contentDescription = "Go to the list top",
-                    tint = Color.Black,
-                    modifier = Modifier.size(24.dp)
-                )
-            }
+            ),
+            arguments = listOf(
+                navArgument(name = "id") {
+                    type = NavType.IntType
+                    defaultValue = -1
+                }
+            )
+        ) { entry ->
+            val id = entry.arguments?.getInt("id")
 
-            IconButton(onClick = {
-                scope.launch {
-                    state.animateScrollToItem(index = 99)
-                }
-            }) {
-                Icon(
-                    imageVector = Icons.Default.KeyboardArrowDown,
-                    contentDescription = "Go to the list bottom",
-                    tint = Color.Black,
-                    modifier = Modifier.size(24.dp)
-                )
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Text(text = "The passed number: $id")
             }
         }
     }
